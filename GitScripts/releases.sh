1e97060af79a871213afb984e515f40dcf357eb5
bleronsh <bleronshabiuu@gmail.com>
2023-05-02 13:19:56 +0200

    bleron

--- GitScripts/releases.sh
+++ GitScripts/releases.sh
@@ -1,42 +1,36 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Set the name of your release branch (e.g. "release-v1.0")
-RELEASE_BRANCH_NAME="release-v1.0"
-
-# List of repositories to create release branches in
-REPOSITORIES=("2" "22")
-
-# Clone the script repository
-git clone git@github.com:bleronsh/UtilsRepo.git
-cd script-repository
+RELEASE_BRANCH_NAME="release-v1.3"
+GIT_TOKEN="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"
+# List of repositories to create release branches"
+repos="22 2"
 
 # Loop through the list of repositories and create release branches
-for repo in "${REPOSITORIES[@]}"
+# Clone the repository if it doesn't exist locally
+for repo in $repos
 do
-    # Clone the repository if it doesn't exist locally
-    if [ ! -d "$repo" ]
-    then
-        git clone git@github.com:bleronsh/"$repo".git
-    fi
+  if [! -d "$repo"]
+  then
+    git clone "https://$GIT_TOKEN@github.com/bleronsh/$repo.git"
+  fi
+# Change directory to the repository
+  cd "$repo"
 
-    # Change directory to the repository
-    cd "$repo"
-
-    # Checkout to your main branch (e.g. "main" or "master")
-    git checkout main
+    # Checkout to your main branch
+  git checkout main
 
     # Pull the latest changes from the main branch
-    git pull origin main
+  git pull origin main
 
     # Create a new release branch from the main branch
-    git branch "$RELEASE_BRANCH_NAME"
+  git branch "$RELEASE_BRANCH_NAME"
 
     # Checkout to the new release branch
-    git checkout "$RELEASE_BRANCH_NAME"
+  git checkout "$RELEASE_BRANCH_NAME"
 
-    # Push the new release branch to the remote repository
-    git push origin "$RELEASE_BRANCH_NAME"
-
-    # Change directory back to the script repository
-    cd ..
+     # Push the new release branch to the remote repository
+  git push origin "$RELEASE_BRANCH_NAME"
+  cd ..
 done
+# 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
